7fc2be82cc431dce394e4d316ff64ae2d15b581a
Frank Hu <44072346+FYYHU@users.noreply.github.com>
2023-03-05 20:33:01 +0100

    fix brg ->RGB conversion

--- dashboard.py
+++ dashboard.py
@@ -6,14 +6,15 @@ import cv2
 #streamlit stream using webcam
 def streamlit_webcam():
     st.title("Webcam Stream")
-    run = st.checkbox('Run')
+    run = st.checkbox('Run', value=True)
     FRAME_WINDOW = st.image([])
     camera = cv2.VideoCapture(0)
     while run:
         _, frame = camera.read()
+        #convert BRG to RGB
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         FRAME_WINDOW.image(frame)
     del(camera)
+st.title("Streamlit WebCam")
 
-st.title("Streamlit Dashboard")
-#stream the webcam
 streamlit_webcam()
