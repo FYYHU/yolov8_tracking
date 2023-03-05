11dad161ef8ef6adbfc3904fdebe520230063c99
Frank Hu <44072346+FYYHU@users.noreply.github.com>
2023-03-05 20:02:25 +0100

    stream webcam

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,19 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import cv2
 
-st.title('My first app')
+#streamlit stream using webcam
+def streamlit_webcam():
+    st.title("Webcam Stream")
+    run = st.checkbox('Run')
+    FRAME_WINDOW = st.image([])
+    camera = cv2.VideoCapture(0)
+    while run:
+        _, frame = camera.read()
+        FRAME_WINDOW.image(frame)
+    del(camera)
+
+st.title("Streamlit Dashboard")
+#stream the webcam
+streamlit_webcam()
